0b19621ca5dd78bc7ecf2d84c9923ee81eb19e13
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2022-10-20 18:20:59 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,183 @@
+import NextLink from 'next/link'
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+import thumbYouTube from '../public/images/links/youtube.png'
+import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
+import Image from 'next/image'
+
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
+})
+
+const Home = () => (
+  <Layout>
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I&apos;m an aspiring Software Engineer based in California!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Jan William Haug
+          </Heading>
+          <p>Undergraduate senior <br></br>( Software Engineer / Developer / Programmer )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/williamPic.png"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
+        </Box>
+      </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          I am originally from Norway, and currently a senior 
+          Software Engineering student at California State University San Marcos. 
+          Collaborative team member who enjoys problem solving, finding solutions 
+          and working in a team environment. My current programming language skills 
+          are in Java, C++, F# and Python.My LinkedIn is {' '}
+          <NextLink href="/works/inkdrop" passHref scroll={false}>
+            <Link>Inkdrop</Link>
+          </NextLink>
+          &quot;
+          <NextLink href="https://www.linkedin.com/in/janwilliamhaug/" passHref>
+            <Link target="_blank">www.linkedin.com/in/janwilliamhaug/</Link>
+          </NextLink>
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works" passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1998</BioYear>
+          Born in Oslo, Norway.
+        </BioSection>
+        <BioSection>
+          <BioYear>2023</BioYear>
+          BS Software Engineering
+          California State University San Marcos
+          Graduation date 2023 (expected)
+        </BioSection>
+        
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Listening to music,{' '}
+            video games
+          collaborating on projects{' '}
+            watching anime, hiking,
+           Working out
+        </Paragraph>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/JanWilliamHaug" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoGithub />}
+              >
+              
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="https://www.youtube.com/devaslife"
+            title="Dev as Life"
+            thumbnail={thumbYouTube}
+          >
+            More to come
+          </GridItem>
+          <GridItem
+            href="https://www.inkdrop.app/"
+            title="Inkdrop"
+            thumbnail={thumbInkdrop}
+          >
+            My future app
+          </GridItem>
+        </SimpleGrid>
+
+        <Box align="center" my={4}>
+          <NextLink href="/posts" passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My future projects here
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+    </Container>
+  </Layout>
+)
+
+export default Home
+export { getServerSideProps } from '../components/chakra'
